57eab6d7134c49b8ecbc10637eefb0a5c2fe59c5
sawa-ko <56084970+sawa-ko@users.noreply.github.com>
2024-06-24 17:33:04 -0600

    feat: create tasks in database

--- crates/database/src/entities/task.rs
+++ crates/database/src/entities/task.rs
@@ -1,9 +1,10 @@
+use rocket::serde::Serialize;
 use sea_orm::entity::prelude::*;
 
-#[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel)]
+#[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel, Serialize)]
 #[sea_orm(table_name = "tasks")]
 pub struct Model {
-    #[sea_orm(primary_key)]
+    #[sea_orm(primary_key, auto_increment=true)]
     pub id: i32,
     pub name: String,
     pub description: String,
--- src/routes/task.rs
+++ src/routes/task.rs
@@ -2,6 +2,11 @@ use rocket::{delete, form, FromForm, get, patch, post};
 use rocket::form::{Error, Form};
 use rocket::serde::{Deserialize, Serialize};
 use rocket::serde::json::Json;
+use sea_orm::ActiveValue::Set;
+use sea_orm::{EntityTrait};
+use sea_orm_rocket::Connection;
+use database::Db;
+use database::entities::task as Task;
 
 use crate::routes::ResponseRequest;
 
@@ -15,12 +20,36 @@ pub struct ManageTodo {
     is_complete: bool
 }
 
-#[post("/create", data = "<todo>")]
-pub async fn create_task(todo: Form<ManageTodo>) -> Json<ResponseRequest<ManageTodo>> {
+#[post("/create", data = "<form>")]
+pub async fn create_task(form: Form<ManageTodo>, conn: Connection<'_, Db>) -> Json<ResponseRequest<Option<Task::Model>>> {
+    let db = conn.into_inner();
+    let todo = form.into_inner();
+    
+    let task = Task::ActiveModel {
+        id: Default::default(),
+        name: Set(todo.name.trim().to_owned()),
+        description: Set(todo.description.trim().to_owned()),
+        is_active: Set(todo.is_complete.to_owned()),
+    };
+    
+    let task_result = Task::Entity::insert(task).exec(db).await;
+    
+    if task_result.is_err() {
+        let res = ResponseRequest {
+          message: Some("Failed to create task".to_string()),
+          status: 500,
+          data: None
+        };
+        
+        return Json(res);
+    }
+
+    let new_task = Task::Entity::find_by_id(task_result.unwrap().last_insert_id).one(db).await;
+    
     let res = ResponseRequest {
         message: Some("Task created successfully".to_string()),
         status: 200,
-        data: todo.into_inner()
+        data: new_task.unwrap()
     };
 
     Json(res)
