e054213396296a06c6a47b320866bbfb01791cfa
sawa-ko <56084970+sawa-ko@users.noreply.github.com>
2024-06-25 12:50:13 -0600

    feat: add create user table migration

--- crates/migrations/src/lib.rs
+++ crates/migrations/src/lib.rs
@@ -1,12 +1,16 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20240624_213509_task_create;
+mod m20240625_184538_user_create;
 
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(m20240624_213509_task_create::Migration)]
+        vec![
+            Box::new(m20240624_213509_task_create::Migration),
+            Box::new(m20240625_184538_user_create::Migration),
+        ]
     }
 }
