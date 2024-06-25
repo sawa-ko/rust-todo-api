97864d063252705be02c60a655b4a9084e6378f4
sawa-ko <56084970+sawa-ko@users.noreply.github.com>
2024-06-25 10:43:52 -0600

    chore: nothing relevant

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,9 +1,9 @@
-use serde::Serialize;
+use rocket::serde::{Deserialize, Serialize};
 
 pub mod ping;
 pub mod task;
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize)]
 pub struct ResponseRequest<T> {
     message: Option<String>,
     status: u16,
