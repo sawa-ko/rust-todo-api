4e602143ac270d2d99aff43803aa13e723cd509b
sawa-ko <56084970+sawa-ko@users.noreply.github.com>
2024-06-25 12:44:25 -0600

    feat: add initial user entity

---
+++ crates/database/src/entities/user.rs
@@ -0,0 +1,17 @@
+use rocket::serde::{Deserialize, Serialize};
+use sea_orm::entity::prelude::*;
+
+#[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel, Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+#[sea_orm(table_name = "users")]
+pub struct Model {
+    #[sea_orm(primary_key, auto_increment = true)]
+    pub id: i32,
+    pub username: String,
+    pub password: String,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {}
+
+impl ActiveModelBehavior for ActiveModel {}
