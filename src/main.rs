429468a14833d2e37d31974ff39fac43c2f0be86
sawa-ko <56084970+sawa-ko@users.noreply.github.com>
2024-06-26 12:42:14 -0600

    chore: code cleanup

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,7 @@ mod routes;
 
 use database::Db;
 use migration::MigratorTrait;
-use rocket::fairing::AdHoc;
-use rocket::{fairing, routes, Build, Rocket};
+use rocket::{routes};
 use sea_orm_rocket::Database;
 use std::env;
 
@@ -11,12 +10,12 @@ use crate::routes::auth::{me, sign_in, sign_up};
 use crate::routes::ping::ping_route;
 use crate::routes::task::{create_task, delete_task, get_task, get_tasks, update_task};
 
-async fn run_migrations(rocket: Rocket<Build>) -> fairing::Result {
+/*async fn run_migrations(rocket: Rocket<Build>) -> fairing::Result {
     let conn = &Db::fetch(&rocket).unwrap().conn;
     let _ = migration::Migrator::up(conn, None).await;
     Ok(rocket)
 }
-
+*/
 #[tokio::main]
 async fn start_api() -> Result<(), rocket::Error> {
     let figment = rocket::Config::figment().merge((
